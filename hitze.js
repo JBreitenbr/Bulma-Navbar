dcf52b69d1904ccf2a86612ca0942869d4227349
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2026-07-26 15:17:51 +0200

    Update hitze.js

--- hitze.js
+++ hitze.js
@@ -1,7 +1,7 @@
 function showMonth(period) {
   const margin = { top: 20, right: 25, bottom: 30, left: 140 };
-  const outerWidth = 460;
-  const outerHeight = 700;
+  const outerWidth = 800;
+  const outerHeight = 460;
   const width = outerWidth - margin.left - margin.right;
   const height = outerHeight - margin.top - margin.bottom;
 
